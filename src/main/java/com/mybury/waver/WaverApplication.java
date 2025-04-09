c3ce360c9367abd6cc29280b2bf0e74a52e20568
김하늘 <81543341+haneulch@users.noreply.github.com>
2025-04-09 21:33:22 +0900

    init commit

---
+++ src/main/java/com/mybury/waver/WaverApplication.java
@@ -0,0 +1,13 @@
+package com.mybury.waver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mybury/waver/WaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybury.waver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
