f388b0b90b8bc7e5d461b6846b28966094d8b207
Neo <neo.lee@stagefive.com>
2025-08-16 01:07:34 +0900

    feat: ResultCode에 FORBIDDEN 추가 및 Comment 엔티티에 Setter 추가, CommentRepository에 findByIdAndUserId 메서드 추가

--- src/main/java/com/mybury/waver/repository/CommentRepository.java
+++ src/main/java/com/mybury/waver/repository/CommentRepository.java
@@ -1,8 +1,13 @@
 package com.mybury.waver.repository;
 
 import com.mybury.waver.domain.Comment;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Optional<Comment> findByIdAndUserId(Long id, Long userId);
 
 }
