c53d94f082b421bbefdc79ed0a8fa0472e361dfd
Neo <neo.lee@stagefive.com>
2025-08-17 14:24:06 +0900

    feat: 피드 목록 조회 시 좋아요 여부 조회 추가

--- src/main/java/com/mybury/waver/repository/LikeBucketRepository.java
+++ src/main/java/com/mybury/waver/repository/LikeBucketRepository.java
@@ -2,9 +2,13 @@ package com.mybury.waver.repository;
 
 import com.mybury.waver.domain.LikeBucket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Set;
 
 public interface LikeBucketRepository extends JpaRepository<LikeBucket, Long> {
   boolean existsByUserIdAndBucketId(long userId, long bucketId);
 
   void deleteByUserIdAndBucketId(long userId, long bucketId);
+  
+  List<LikeBucket> findByUserIdAndBucketIdIn(long userId, Set<Long> bucketIds);
 }
