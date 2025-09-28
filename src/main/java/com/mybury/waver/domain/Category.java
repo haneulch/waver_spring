1d90d64baba66bdb03d171ef0132c05a9840fb24
haneulch <khaneulc@kakao.com>
2025-09-28 14:53:08 +0900

    [fix] FK Constraint 제거

--- src/main/java/com/mybury/waver/domain/Category.java
+++ src/main/java/com/mybury/waver/domain/Category.java
@@ -2,24 +2,25 @@ package com.mybury.waver.domain;
 
 import com.mybury.waver.common.code.YesNo;
 import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
 
 import static com.mybury.waver.common.Constants.DEFAULT_CATEGORY;
 
@@ -31,34 +32,34 @@ import static com.mybury.waver.common.Constants.DEFAULT_CATEGORY;
 @Builder
 public class Category extends BaseEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  private String name;
+    private String name;
 
-  private Long userId;
+    private Long userId;
 
-  private int seq;
+    private int seq;
 
-  @Builder.Default
-  @Enumerated(value = EnumType.STRING)
-  @Column(length = 1, nullable = false)
-  private YesNo deleted = YesNo.N;
+    @Builder.Default
+    @Enumerated(value = EnumType.STRING)
+    @Column(length = 1, nullable = false)
+    private YesNo deleted = YesNo.N;
 
-  @Builder.Default
-  @Enumerated(value = EnumType.STRING)
-  @Column(length = 1, nullable = false)
-  private YesNo defaultYn = YesNo.N;
+    @Builder.Default
+    @Enumerated(value = EnumType.STRING)
+    @Column(length = 1, nullable = false)
+    private YesNo defaultYn = YesNo.N;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "userId", insertable = false, updatable = false)
-  private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", insertable = false, updatable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private User user;
 
-  @OneToMany(fetch = FetchType.LAZY, mappedBy = "category")
-  private List<Bucket> buckets;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "category")
+    private List<Bucket> buckets;
 
-  public static Category createDefaultCategoryFor(long userId) {
-    return Category.builder().name(DEFAULT_CATEGORY).defaultYn(YesNo.Y).userId(userId).build();
-  }
+    public static Category createDefaultCategoryFor(long userId) {
+        return Category.builder().name(DEFAULT_CATEGORY).defaultYn(YesNo.Y).userId(userId).build();
+    }
 }
