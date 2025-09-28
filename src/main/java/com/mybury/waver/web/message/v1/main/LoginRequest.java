8153f1ccfc07f0cd585a7670073119e5f953c8e6
haneulch <khaneulc@kakao.com>
2025-09-28 16:45:51 +0900

    [fix] 로그인처리시 email 제거

--- src/main/java/com/mybury/waver/web/message/v1/main/LoginRequest.java
+++ src/main/java/com/mybury/waver/web/message/v1/main/LoginRequest.java
@@ -1,15 +1,8 @@
 package com.mybury.waver.web.message.v1.main;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
-    @Email
-    @NotBlank
-    @Schema(description = "이메일")
-    String email,
-
     @NotBlank
     String uid
 ) {
