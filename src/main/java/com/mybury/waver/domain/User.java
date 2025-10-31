7b32af83d0f0cc7a085dcd1f2a1543738f6a8d53
haneulch <khaneulc@kakao.com>
2025-10-31 21:49:40 +0900

    [feat] 프로필 수정 개발

--- src/main/java/com/mybury/waver/domain/User.java
+++ src/main/java/com/mybury/waver/domain/User.java
@@ -4,15 +4,28 @@ import com.mybury.waver.common.code.AccountType;
 import com.mybury.waver.common.code.PremiumStatus;
 import com.mybury.waver.common.code.UserStatus;
 import com.mybury.waver.common.code.YesNo;
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -23,7 +36,7 @@ public class User extends BaseEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(unique = true, nullable = false)
+  @Column(unique = true, nullable = false, length = 1500)
   private String uid;
 
   @Column(nullable = false)
