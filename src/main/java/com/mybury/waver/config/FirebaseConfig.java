6bd0914a821c3164808578d10f4c9a4c7de0ad2b
haneulch <khaneulc@kakao.com>
2026-06-28 22:52:52 +0900

    feat: Change subscription id

--- src/main/java/com/mybury/waver/config/FirebaseConfig.java
+++ src/main/java/com/mybury/waver/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
@@ -11,11 +12,16 @@ import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
+
+  @Value("${waver.service-key-path}")
+  private String serviceKeyPath;
+
+
   @PostConstruct
   public void init() {
     try {
       FileInputStream serviceAccount =
-          new FileInputStream("src/main/resources/waver-service-account.json");
+          new FileInputStream(serviceKeyPath);
 
       FirebaseOptions options = FirebaseOptions.builder()
           .setCredentials(GoogleCredentials.fromStream(serviceAccount))
